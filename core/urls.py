0bd46ef40a60ade2eb0b7c001e57b6f563c4f9aa
Adam M Peacock <adam@thepeacock.net>
2016-02-23 09:03:18 -0500

    Renamed peacock_invoices to core

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,4 @@
-"""peacock_invoices URL Configuration
+"""core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
